f7b77a3c80526defe4a2f98534d4f6f2a50f502d
Christian <christian.kreiling@gmail.com>
2017-10-18 16:46:09 -0400

    Ready to implement scheduler

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
 from flask import Flask
+import os
+import requests
 
 app = Flask(__name__)
+
+bot_id = os.getenv('BOT_ID', '')  # second variable is default, nothing
+token = os.getenv('USER_TOKEN', '')
+
+"""
+Base URL for Groupme's API
+"""
+baseUrl = 'https://api.groupme.com/v3'
+
+"""
+Post material:
+    - bot_id: ID for the bot
+    - text: text for the bot to send to the group
+"""
+botPostMessageUrl = '/bots/post'
+
+
+def post_message_to_group(text):
+    """
+    Posts a message to the group
+    :param text: message to be posted to group
+    :return: status code of the request
+    """
+    url = baseUrl + botPostMessageUrl + '/' + token
+    json = {bot_id, text}
+    response = requests.post(url, json=json)
+    return response.status_code
 
 
 @app.route('/')
 def index():
+    """
+    All messages are sent to this URL and processed
+    """
     return 'Yo, it\'s working!'
 
 
